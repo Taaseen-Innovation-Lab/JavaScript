0fa116e5fc56ad52f6e903cffabad4e0981ac765
Taaseen-Innovation-Lab <muhammadtaaseen123@gmail.com>
2020-07-21 18:35:51 +0500

    type of operatror

---
+++ Lesson 1/Type_of_operator/index.js
@@ -0,0 +1,11 @@
+
+var age = 64;
+var name = "hello"
+var isFeePaid = true
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isFeePaid)
+
+isFeePaid = 56;
+
+console.log(typeof isFeePaid)
